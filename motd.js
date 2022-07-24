27ebf3050bbae47d581128a4cea718caf4025ba6
czarhex <ferenc.kolesa@gmail.com>
2022-07-24 18:34:19 +0200

    message of the day

---
+++ motd.js
@@ -0,0 +1,11 @@
+var r_text = new Array (); 
+r_text[0] = "I am hungry."; 
+r_text[1] = "Religion is retarded."; 
+r_text[2] = "Sick bro."; 
+r_text[3] = "Always give up."; 
+r_text[4] = "Don't."; 
+
+var i = Math.floor(r_text.length * Math.random()); 
+
+document.write("<title>" + r_text[i]  + "</title>"); 
+
