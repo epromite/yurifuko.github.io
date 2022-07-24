67b3f7dcea35e849b8b457eaadba6ff45c8bd14e
czarhex <czarhex@protonmail.com>
2022-07-24 18:55:37 +0200

    motd

--- motd.js
+++ motd.js
@@ -1,9 +1,17 @@
 var r_text = new Array (); 
-r_text[0] = "I am hungry."; 
-r_text[1] = "Religion is retarded."; 
-r_text[2] = "Sick bro."; 
-r_text[3] = "Always give up."; 
+r_text[0] = "I am hungry"; 
+r_text[1] = "Religion is retarded"; 
+r_text[2] = "Sick bro"; 
+r_text[3] = "Always give up"; 
 r_text[4] = "Don't."; 
+r_text[5] = "Saul Goodman Rule34"; 
+r_text[6] = "Did you know that you have rights?"; 
+r_text[7] = "Sempai?"; 
+r_text[8] = "Kid named finger:"; 
+r_text[9] = "We do a little trolling"; 
+r_text[10] = "Mood."; 
+r_text[11] = "Holding your family hostage"; 
+r_text[12] = "Walking on sunshine"; 
 
 var i = Math.floor(r_text.length * Math.random()); 
 
