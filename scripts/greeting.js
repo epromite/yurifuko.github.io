6851f348d5342888757eee3f59399d9e86442b84
czarhex <ferenc.kolesa@gmail.com>
2022-07-28 12:31:31 +0200

    update

---
+++ scripts/greeting.js
@@ -0,0 +1,18 @@
+var r_text = new Array (); 
+r_text[0] = "Hi there!"; 
+r_text[1] = "Hey!"; 
+r_text[2] = "What's up?"; 
+r_text[3] = "Hello there!"; 
+r_text[4] = "こんにちは!"; 
+r_text[5] = "你好!"; 
+r_text[6] = "Bonjour!"; 
+r_text[7] = "Hola!"; 
+r_text[8] = "Ciao!"; 
+r_text[9] = "Привіт!"; 
+r_text[10] = "Ahoj!"; 
+r_text[11] = "안녕하세요!"; 
+r_text[12] = "Hallo!";
+
+var i = Math.floor(r_text.length * Math.random()); 
+
+document.write("<h1>" + r_text[i]  + " 👋</h1>"); 
