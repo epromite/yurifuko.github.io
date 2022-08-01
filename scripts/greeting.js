36aa1b4274cc9e242fc731784e1e5d86039cc1a4
czarhex <ferenc.kolesa@gmail.com>
2022-08-01 03:28:53 +0200

    love you fizzify

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,17 +1,4 @@
-var r_text = new Array (); 
-r_text[0] = "Hi there!"; 
-r_text[1] = "Hey!"; 
-r_text[2] = "What's up?"; 
-r_text[3] = "Hello there!"; 
-r_text[4] = "こんにちは!"; 
-r_text[5] = "你好!"; 
-r_text[6] = "Bonjour!"; 
-r_text[7] = "Hola!"; 
-r_text[8] = "Ciao!"; 
-r_text[9] = "Привіт!"; 
-r_text[10] = "Ahoj!"; 
-r_text[11] = "안녕하세요!"; 
-r_text[12] = "Hallo!";
+var r_text = ["Hi there!", "Hey!", "What's up?", "Hello there!", "こんにちは!", "你好!", "Bonjour!", "Hola!", "Ciao!", "Привіт!", "Ahoj!", "안녕하세요!", "Hallo!",];
 
 var i = Math.floor(r_text.length * Math.random()); 
 
