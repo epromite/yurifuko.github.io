3292696d829091bd681c44cd0dc15c8f5e6f5fd2
czarhex <czarhex@protonmail.com>
2022-08-03 00:04:29 +0200

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,4 +1,4 @@
-var r_text = ["Hi there!", "Hey!", "What's up?", "Hello there!", "こんにちは!", "你好!", "Bonjour!", "Hola!", "Ciao!", "Привіт!", "Ahoj!", "안녕하세요!", "Hallo!",];
+var r_text = ["Hi there!", "Hey!", "What's up?", "Hello there!", "こんにちは!", "你好!", "Bonjour!", "Hola!", "Ciao!", "Привіт!", "Ahoj!", "안녕하세요!", "Hallo!", "Cześć!", "Hallå!", "Zdravo!", "Szia!"];
 
 var i = Math.floor(r_text.length * Math.random()); 
 
