e2747565a756c62a0e544c968742b01c5d0be383
czarhex <czarhex@protonmail.com>
2022-08-14 22:03:42 +0200

    Update motd.js

--- scripts/motd.js
+++ scripts/motd.js
@@ -1,4 +1,4 @@
-var r_text = ["I am hungry", "Religion is retarded", "Sick bro", "Always give up", "Don't.", "Saul Goodman Rule34", "Did you know that you have rights?", "Sempai?", "Kid named finger:", "We do a little trolling", "Mood.", "Holding your family hostage", "Walking on sunshine", "A, bobr kurwa!", "You Like Huey Lewis And The News?", "June 4th 1989", "Gigachad's portal", "Panzer vor!", "It's rentin' time!", "There are only 2 genders", "Don't care.", "A lie told often becomes the truth", "I choose violence"];
+var r_text = ["I am hungry", "Religion is retarded", "Sick bro", "Always give up", "Don't.", "Saul Goodman Rule34", "Nazi anime waifus are the best", "Sempai?", "Kid named finger:", "We do a little trolling", "Mood.", "Holding your family hostage", "Walking on sunshine", "A, bobr kurwa!", "You Like Huey Lewis And The News?", "June 4th 1989", "Gigachad's portal", "Panzer vor!", "It's rentin' time!", "There are only 2 genders", "Don't care.", "A lie told often becomes the truth", "I choose violence"];
 
 var i = Math.floor(r_text.length * Math.random()); 
 
