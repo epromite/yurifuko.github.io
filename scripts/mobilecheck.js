a075b62f3b87517c515c09a07728538726656109
yurifuko <ferenc.kolesa@gmail.com>
2022-08-26 15:50:22 +0200

    cool

--- scripts/mobilecheck.js
+++
@@ -1,6 +0,0 @@
-if ("ontouchstart" in document.documentElement) {
-  //window.location.replace("mobile");
-  alert("you are on mobile fuck you (the website looks better on desktop)");
-} else {
-  null;
-}
