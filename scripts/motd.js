0cc6c2668d83a80fa5221f6a8b17406e117200a7
yurifuko <ferenc.kolesa@gmail.com>
2022-08-27 11:36:43 +0200

    my little phonies <3

--- scripts/motd.js
+++ scripts/motd.js
@@ -1,5 +1,5 @@
-var r_text = ["I am hungry", "Religion is retarded", "Sick bro", "Always give up", "Don't.", "Saul Goodman Rule34", "Nazi anime waifus are the best", "Sempai?", "Kid named finger:", "We do a little trolling", "Mood.", "Holding your family hostage", "Walking on sunshine", "A, bobr kurwa!", "You Like Huey Lewis And The News?", "June 4th 1989", "Gigachad's portal", "Panzer vor!", "It's rentin' time!", "There are only 2 genders", "Don't care.", "A lie told often becomes the truth", "I choose violence", "Lick my ass onii-chan", "You suck.", "Squiddin' awesome", "Aw shit nigga", "I want to fit in"];
+var r_text = ["I am hungry", "Religion is retarded", "Sick bro", "Always give up", "Don't.", "Nazi anime waifus are the best", "Sempai?", "Kid named finger:", "We do a little trolling", "Mood.", "Holding your family hostage", "Walking on sunshine", "A, bobr kurwa!", "You Like Huey Lewis And The News?", "June 4th 1989", "Gigachad's portal", "Panzer vor!", "It's rentin' time!", "There are only 2 genders", "Don't care.", "A lie told often becomes the truth", "I choose violence", "Lick my ass onii-chan", "You suck.", "Squiddin' awesome", "Aw shit nigga", "I want to fit in", "Bitches get stitches"];
 
 var i = Math.floor(r_text.length * Math.random()); 
 
-document.write("<title>" + r_text[i]  + "</title>"); 
+document.write("<p><i>“" + r_text[i]  + "”</i></p>"); 
