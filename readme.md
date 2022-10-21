4e9ff0d59cf498b344da813fec8846c129b1a435
yurifuko <76652465+yurifuko@users.noreply.github.com>
2022-10-21 21:52:50 +0200

    temporary update

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-**`Fonts used`** ・ https://developer.apple.com/fonts/  
+**`Fonts used`** ・ https://fonts.google.com/specimen/Inter  
 **`Icons used`** ・ https://fontawesome.com/icons, https://simpleicons.org
--- scripts/greeting.js
+++
@@ -1,5 +0,0 @@
-var r_text = ["Hi there!", "Hey!", "What's up?", "Hello there!", "こんにちは!", "你好!", "Bonjour!", "Hola!", "Ciao!", "Привіт!", "Ahoj!", "안녕하세요!", "Hallo!", "Cześć!", "Hallå!", "Zdravo!", "Szia!"];
-
-var i = Math.floor(r_text.length * Math.random()); 
-
-document.write("<h1>" + r_text[i]  + " 👋</h1>"); 
