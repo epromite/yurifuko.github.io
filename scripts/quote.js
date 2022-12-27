4532579a45c451de53fd4b928ac6b362c4a91a26
yurifuko <ferenc.kolesa@gmail.com>
2022-12-27 20:07:01 +0100

    what the fuck is this doing here

--- scripts/quote.js
+++
@@ -1,5 +0,0 @@
-var r_text = ["Religion is retarded", "Always give up", "Sorry to keep you waiting.", "Mood.", "What's good, melancholy?", "Walking on sunshine", "Don't care.", "A lie told often becomes the truth.", "I choose violence", "I want to fit in.", "All hope is gone", "The rain will kill us all", "まだまだこれからよ！", "If your first attempt doesn't work, give up", "あぁあぁあぁああぁ。。。", "It's only going to get worse.", "Born to die"];
-
-var i = Math.floor(r_text.length * Math.random()); 
-
-document.write("<q><cite>" + r_text[i]  + "</cite></q>"); 
