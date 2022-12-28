1d499f2440165c41b267f9c837ba33868dc10f85
yurifuko <ferenc.kolesa@gmail.com>
2022-12-28 13:31:26 +0100

    t

--- js/mytime.js
+++ js/mytime.js
@@ -1,29 +1,10 @@
-function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  
-  function startTime() {
-    var mytz = new Date().toLocaleString([], { timeZone: "Europe/Prague" });
-    var now = new Date(mytz);
-  
-    var h = now.getHours();
-    var m = now.getMinutes();
-
-    var isFormat12H = false;
-    var ampm = "";
-    if (isFormat12H) {
-      ampm = h >= 12 ? " PM" : " AM";
-      h = h % 12;
-      h = h ? h : 12;
-    }
-    m = checkTime(m);
-
-    document.getElementById("time").innerHTML = h + ":" + m + ampm;
-    t = setTimeout(function () {
-      startTime();
-    }, 1000);
-  }
-  startTime();
+function startTime() {
+  var mytz = new Date().toLocaleString([], { timeZone: "Europe/Prague" });
+  var now = new Date(mytz);
+  var h = now.getHours();
+  var m = now.getMinutes();
+  m = (m < 10 ? "0" : "") + m;
+  document.getElementById("time").innerHTML = h + ":" + m;
+  setTimeout(startTime, 1000);
+}
+startTime();
